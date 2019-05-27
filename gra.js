403794cf7973293b14047f2f82cfe5109fdd0146
alcadias123 <50618407+alcadias123@users.noreply.github.com>
2019-05-27 21:10:44 +0200

    Update gra.js

--- gra.js
+++ gra.js
@@ -1,1 +1,9 @@
-#jeszcze nic 
+var mcan = document.getElementById('snake');
+var ctx = mcan.getContext('2d');
+var rozmiar = 10;
+var w = 350;
+var h = 350;
+var score = 0;
+var snake;
+var rozmiar = 10;
+var jedzenie;
