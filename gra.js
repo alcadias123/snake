f5d93f8f57e983cbeeda888f6ee4a201dc100a4b
alcadias123 <50618407+alcadias123@users.noreply.github.com>
2019-05-27 21:11:21 +0200

    Update gra.js

--- gra.js
+++ gra.js
@@ -7,3 +7,26 @@ var score = 0;
 var snake;
 var rozmiar = 10;
 var jedzenie;
+var materialy = (function () {
+	var cialo = function (x,y0 {
+		// pojedynczy kwadracik
+		ctx.fillStyle = 'red';
+		ctx.fillRect(x*rozmiar, y*rozmiar, rozmiar, rozmiar);
+		// granica 
+		ctx.strokeStyle = 'darkgreen';
+		ctx.strokeRect(x*rozmiar, y*rozmiar, rozmiar, rozmiar);
+	}
+	var food = function (x, y) {
+		//granica jedzonka
+		ctx.fillStyle = 'purple';
+		ctx.fillRect(x*rozmiar, y*rozmiar, rozmiar, rozmiar);
+		//srodek jedzonka
+		ctx.fillStyle = 'blue';
+        ctx.fillRect(x*rozmiar+1, y*rozmiar+1, rozmiar-2, rozmiar-2);
+	}
+	
+	var tablicapkt = function(){
+		var wynik_tekst = "Score: " + wynik;
+		ctx.fillStyle = 'pink';
+		ctx.fillText(wynik_tekst, 145, h-5);
+	}
